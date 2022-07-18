86fbedf554e641e2cd9bc009e69d12bc8918396d
Leon L. Robinson <leon.robinson@qa.com>
2022-07-18 16:29:26 +0100

    for

---
+++ for_each_example/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+
+variable "imagemap" {
+  default = { 0 = "ami-078a289ddf4b09ae0"
+  1 = "ami-0bd2099338bc55e6d" }
+}
+
+
+resource "aws_instance" "my_instance" {
+  for_each = var.imagemap
+  ami      = each.value
+  instance_type     = "t2.micro"
+  tags = {
+    Name = "my_instance"
+  }
+
+}
